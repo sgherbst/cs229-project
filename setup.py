fb49da04eb0202090da73e46871e515d5c419c72
Steven Herbst <sherbst@stanford.edu>
2018-11-18 13:57:45 -0800

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cs229',
+    version='0.0.1',
+    description='CS229 Project',
+    url='https://github.com/sgherbst/cs229-project',
+    author='Steven Herbst',
+    author_email='sherbst@stanford.edu',
+    packages=['cs229'],
+    install_requires=[
+        'scipy',
+        'numpy'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
